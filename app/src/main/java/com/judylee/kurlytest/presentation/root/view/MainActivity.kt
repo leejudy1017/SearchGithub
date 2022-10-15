fdf59e57807741197fb6a96fea6eee2628da07ae
jihyun <leejudy1017@naver.com>
2022-10-16 00:34:39 +0900

    add : recyclerview RepositoryAdapter 연결

--- app/src/main/java/com/judylee/kurlytest/presentation/root/view/MainActivity.kt
+++ app/src/main/java/com/judylee/kurlytest/presentation/root/view/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.judylee.kurlytest.presentation.root.view
 
+import android.content.Context
 import android.util.Log
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.judylee.kurlytest.R
-import com.judylee.kurlytest.data.network.ApiService
 import com.judylee.kurlytest.data.network.RetrofitApi
 import com.judylee.kurlytest.data.network.dto.SearchGithubResponse
 import com.judylee.kurlytest.databinding.ActivityMainBinding
 import com.judylee.kurlytest.presentation.base.BaseActivity
+import com.judylee.kurlytest.presentation.root.view.adapter.RepositoryAdapter
 import com.judylee.kurlytest.presentation.root.viewModel.MainViewModel
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,9 +22,10 @@ import retrofit2.Response
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private val mainVM : MainViewModel by lazy {
+    private val mainVM: MainViewModel by lazy {
         ViewModelProvider(this)[MainViewModel::class.java]
     }
+
     override fun extraSetupBinding() {
         binding.vm = mainVM
     }
@@ -29,22 +36,59 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun setupViews() {
 
-        val call : Call<SearchGithubResponse> = RetrofitApi.apiService.getGitHubRepositories("android")
-        call.enqueue(object : Callback<SearchGithubResponse> {
-            override fun onResponse(
-                call: Call<SearchGithubResponse>,
-                response: Response<SearchGithubResponse>
-            ) {
-                Log.i("success",response.body()?.items.toString())
-                Log.i("success",response.body()?.totalCount.toString())
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(text: String?): Boolean {
+                // 키보드 내리고 searchView 포커싱 없애기
+                hideKeyboard()
+                binding.searchView.clearFocus()
+                if (text != null) {
+                    if (text.isNotEmpty()) {
+                        Log.i("text", text)
+                        //Todo: 네트워크 확인
+                        //Todo: 로딩스피너
+                        val call: Call<SearchGithubResponse> =
+                            RetrofitApi.apiService.getGitHubRepositories(text)
+                        call.enqueue(object : Callback<SearchGithubResponse> {
+                            override fun onResponse(
+                                call: Call<SearchGithubResponse>,
+                                response: Response<SearchGithubResponse>
+                            ) {
+                                Log.i("success", response.body()?.items.toString())
+                                Log.i("success", response.body()?.totalCount.toString())
+
+                                if (response.body()?.totalCount == 0) {
+                                    // 안내문구 (검색 정보가 없을 때)
+                                    Toast.makeText(applicationContext, "검색된 리포지토리가 없습니다.", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    var listManager = LinearLayoutManager(applicationContext, RecyclerView.VERTICAL, false)
+                                    var listAdapter = response.body()?.let { RepositoryAdapter(applicationContext, text, it) }
+                                    var recyclerList = binding.recyclerView.apply {
+                                        setHasFixedSize(true)
+                                        layoutManager = listManager
+                                        adapter = listAdapter
+                                    }
+                                }
+                            }
+
+                            override fun onFailure(call: Call<SearchGithubResponse>, t: Throwable) {
+                                Log.i("fail", t.toString())
+                            }
+
+                        })
+                    }
+                    else {
+                        // 안내문구 (키보드 입력) : length = 0
+                        Toast.makeText(applicationContext, "검색어를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                return true
             }
 
-            override fun onFailure(call: Call<SearchGithubResponse>, t: Throwable) {
-                Log.i("fail",t.toString())
+            override fun onQueryTextChange(text: String?): Boolean {
+                return true
             }
 
         })
-
 
     }
 
@@ -52,6 +96,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     }
 
     override fun release() {
+    }
+
+    fun hideKeyboard() {
+        val imm = applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(this.currentFocus?.windowToken, 0)
     }
 
 }
---
+++ app/src/main/java/com/judylee/kurlytest/presentation/root/view/adapter/RepositoryAdapter.kt
@@ -0,0 +1,63 @@
+package com.judylee.kurlytest.presentation.root.view.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.judylee.kurlytest.R
+import com.judylee.kurlytest.data.network.dto.SearchGithubResponse
+
+class RepositoryAdapter(var context : Context, var searchText : String, var data : SearchGithubResponse) : RecyclerView.Adapter<RepositoryAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.github_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        Glide.with(context)
+            .load(data.items[position].owner.avatarUrl)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .fitCenter()
+            .circleCrop()
+            .into(holder.ivUser)
+
+        //Todo : searchText 와 동일한 부분 Bold 처리
+        holder.tvTitle.text = data.items[position].fullName
+        holder.tvDescription.text = data.items[position].description
+        if(data.items[position].description == ""){
+            holder.tvDescription.visibility = View.GONE
+        }
+        else{
+            holder.tvDescription.visibility = View.VISIBLE
+        }
+        holder.tvStarCount.text = data.items[position].stargazersCount.toString()
+
+        holder.itemView.setOnClickListener {
+            //Todo : 링크 연결
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+       return data.totalCount
+    }
+
+    class ViewHolder(v: View): RecyclerView.ViewHolder(v){
+        private var view : View = v
+        var ivUser = v.findViewById<ImageView>(R.id.ivUser)
+        var tvTitle = v.findViewById<TextView>(R.id.tvTitle)
+        var tvDescription = v.findViewById<TextView>(R.id.tvDescription)
+        var tvStarCount = v.findViewById<TextView>(R.id.tvStarCount)
+    }
+
+}
